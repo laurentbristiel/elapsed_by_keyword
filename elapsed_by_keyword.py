1e6dca1ab81dd60ed53dda472f7d8d4d404a2947
apallier <a.pallier@laposte.net>
2019-02-25 21:32:39 +0100

    Add keyword occurence information
    Other minor enhancements:
    
    * Output format compatible with Excel (tabulation used)
    * Add Python 3 compatibility
    * Add test data

--- elapsed_by_keyword.py
+++ elapsed_by_keyword.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """Usage: elapsed_by_keyword path/to/output.xml 
 
 Reads result of a test run from Robot output file
@@ -7,31 +6,37 @@ and outputs "keyword elapsed_time"
 sorted by the elapsed time in each keyword.
 """
 
-import sys
 import datetime
+import sys
+
 from robot.api import ExecutionResult, ResultVisitor
 
-class ExecutionKeywordStats(ResultVisitor):
 
+class ExecutionKeywordStats(ResultVisitor):
     def __init__(self):
-        self.elapsed_by_kw = {}
+        self.stats_by_kw = {}
 
     def end_keyword(self, keyword):
-        if keyword.name in self.elapsed_by_kw:
-            self.elapsed_by_kw[keyword.name] = self.elapsed_by_kw[keyword.name] + keyword.elapsedtime
+        if keyword.name in self.stats_by_kw:
+            self.stats_by_kw[keyword.name]['elapsedtime'] += keyword.elapsedtime
+            self.stats_by_kw[keyword.name]['count'] += 1
         else:
-            self.elapsed_by_kw[keyword.name] = keyword.elapsedtime
+            self.stats_by_kw[keyword.name] = {'elapsedtime': keyword.elapsedtime, 'count': 1}
+
 
 def stats_by_keywords(path):
-    
+
     result = ExecutionResult(path)
-    visitor = ExecutionKeywordStats()    
+    visitor = ExecutionKeywordStats()
     result.visit(visitor)
-    for kw in sorted(visitor.elapsed_by_kw, key=visitor.elapsed_by_kw.get, reverse=True):
-      print kw, str(datetime.timedelta(milliseconds=visitor.elapsed_by_kw[kw]))[:-7]
+    for kw in sorted(visitor.stats_by_kw, key=lambda kw: visitor.stats_by_kw[kw]['elapsedtime'], reverse=True):
+        print('{0}\t{1}\t{2}'.format(kw,
+                                     str(datetime.timedelta(milliseconds=visitor.stats_by_kw[kw]['elapsedtime']))[:-7],
+                                     visitor.stats_by_kw[kw]['count']))
+
 
 if __name__ == '__main__':
     try:
         stats_by_keywords(*sys.argv[1:])
     except TypeError:
-        print __doc__
+        print(__doc__)
